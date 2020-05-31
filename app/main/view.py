fc32fe862c54c9daac78eea908a213728c647d33
juma-359 <joeljuma08@gmail.com>
2020-05-31 15:44:50 +0300

    added main decorators that will take the new and articles to display on the news page

---
+++ app/main/view.py
@@ -0,0 +1,16 @@
+from flask import render_template
+from . import main
+from ..request import get_source,get_articles
+
+@main.route('/')
+def index():
+    news_source=get_source()
+    print(news_sources)
+    title='Welcome to the new highlighter'
+    return render_template('index.html', title=title, source=news_sources)
+
+@main.route('/articles/<string:id>')
+def source(id):
+    articles=get.get_articles(id)
+    print(articles)
+    return render_template('articles.html',articles=articles)

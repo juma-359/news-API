853cce0e7da0e306ed85cd1a66647e299eab6e29
juma-359 <joeljuma08@gmail.com>
2020-05-31 15:34:43 +0300

    this will hundle the requests send to the server

---
+++ manage.py
@@ -0,0 +1,18 @@
+from app import creat_app
+from flask_script import Manager,Server
+
+#creating app instance
+app = creat_app('development')
+
+manager = manager(app)
+
+manager.add_command('server',Server)
+@manager.command
+def test():
+    """run the unit tests"""
+    import unittest
+    test=unitnest.TestLoader().discover('tests')
+    unittest.TextRunner(verbosity=2).run(tests)
+
+if __name__=='__main__':
+    manager.run()
